e5559693fb762af912ac459cd16a3c67fa66d76b
tagtwp <153000337+tagtwp@users.noreply.github.com>
2024-05-05 12:55:38 +0700

    Updated the plugin name in the `install-plugins.php` file to `_TWP Core` and adjusted related references.

--- theme/backend/install-plugins.php
+++ theme/backend/install-plugins.php
@@ -11,7 +11,7 @@ if ( ! function_exists( '_twp_register_required_plugins' ) ) {
 
 		$plugins = [
 			[
-				'name'               => esc_html__( 'TWP Core', '_twp' ),
+				'name'               => esc_html__( '_TWP Core', '_twp' ),
 				'slug'               => 'twp-core',
 				'source'             => get_theme_file_path( 'plugins/twp-core.zip' ),
 				'required'           => true,
@@ -48,9 +48,9 @@ if ( ! function_exists( '_twp_register_required_plugins' ) ) {
 		];
 
 		$_twp_config = [
-			'id'           => '_twp',
+			'id'           => _TWP_PREFIX,
 			'default_path' => '',
-			'menu'         => 'twp-plugins',
+			'menu'         => _TWP_PREFIX . '-plugins',
 			'parent_slug'  => 'themes.php',
 			'capability'   => 'edit_theme_options',
 			'has_notices'  => true,
@@ -63,13 +63,13 @@ if ( ! function_exists( '_twp_register_required_plugins' ) ) {
 				'menu_title'                      => esc_html__( 'Install Plugins', '_twp' ),
 				'installing'                      => esc_html__( 'Installing Plugin: %s', '_twp' ),
 				'oops'                            => esc_html__( 'Something went wrong with the plugin API.', '_twp' ),
-				'notice_can_install_required'     => _n_noop( 'TWP the following plugin: %1$s.', 'TWP requires the following plugins: %1$s.', '_twp' ),
-				'notice_can_install_recommended'  => _n_noop( 'TWP recommends the following plugin: %1$s.', 'TWP recommends the following plugins: %1$s.', '_twp' ),
+				'notice_can_install_required'     => _n_noop( '_TWP the following plugin: %1$s.', '_TWP requires the following plugins: %1$s.', '_twp' ),
+				'notice_can_install_recommended'  => _n_noop( '_TWP recommends the following plugin: %1$s.', '_TWP recommends the following plugins: %1$s.', '_twp' ),
 				'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', '_twp' ),
 				'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', '_twp' ),
 				'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', '_twp' ),
 				'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', '_twp' ),
-				'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with TWP: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with TWP: %1$s.', '_twp' ),
+				'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with _TWP: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with _TWP: %1$s.', '_twp' ),
 				'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', '_twp' ),
 				'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', '_twp' ),
 				'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', '_twp' ),
