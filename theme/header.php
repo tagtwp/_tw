a3baa10f935719a978b284bc6c94a0d48763137a
tagtwp <153000337+tagtwp@users.noreply.github.com>
2024-05-05 12:50:08 +0700

    Add localStorage theme saving and auto update

--- theme/header.php
+++ theme/header.php
@@ -16,6 +16,17 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<!-- Theme Check and Update -->
+	<script>
+		const html = document.querySelector('html');
+		const isLightOrAuto = localStorage.getItem('hs_theme') === 'light' || (localStorage.getItem('hs_theme') === 'auto' && !window.matchMedia('(prefers-color-scheme: dark)').matches);
+		const isDarkOrAuto = localStorage.getItem('hs_theme') === 'dark' || (localStorage.getItem('hs_theme') === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+		if (isLightOrAuto && html.classList.contains('dark')) html.classList.remove('dark');
+		else if (isDarkOrAuto && html.classList.contains('light')) html.classList.remove('light');
+		else if (isDarkOrAuto && !html.classList.contains('dark')) html.classList.add('dark');
+		else if (isLightOrAuto && !html.classList.contains('light')) html.classList.add('light');
+	</script>
 	<?php wp_head(); ?>
 </head>
 
